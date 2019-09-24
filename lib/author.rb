138165eaf95154147db6df6a58081da18743f80c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-24 02:49:43 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,7 +14,6 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
-    #post.author
   end
   def self.post_count
     @posts = 3
--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,13 @@
 class Post
   attr_accessor :title, :author
- 
   @@all = []
- 
   def initialize(title)
     @title = title
     save
   end
- 
   def save
     @@all << self
   end
- 
   def self.all
     @@all
   end
